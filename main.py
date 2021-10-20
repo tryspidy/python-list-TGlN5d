e5defb08dfa25c79fd81709e3ca556a72edd94f7
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-20 06:37:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Creating lists
+my_list = [1, "Hello", 3.4, 0, "World"]
+my_nested_list = [['Hello', 'World'],[47,39]]
+
+#Accessing lists
+my_list[1] # Hello
+my_list[-2] # 0
+my_list[:3] # [1, "Hello", 3.4]
+my_nested_list[1] #[47,39]
+my_nested_list[0][1] # World
